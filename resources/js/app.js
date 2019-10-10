f46d137ef0008a15da91ef48912eb5ebb55fe6e4
Mano <r0740200@student.thomasmore.be>
2019-10-10 23:00:34 +0200

    Opdracht 2: <= Automation

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,12 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// Make all functions inside 'vinylShop.js' that start with 'export' accessible inside the HTML pages
+window.vinylShop = require('./vinylShop');
+// Run the hello() function
+vinylShop.hello();
+
+//window.Vue = require('vue');
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +24,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +32,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
+/*const app = new Vue({
     el: '#app',
-});
+});*/
---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+<footer class="page-footer font-small border-top">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">
+        <?php
+            echo "© ",date("Y");
+        ?>
+        <a class="text-secondary" href="https://mdbootstrap.com/education/bootstrap/">The Vinyl Shop</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
